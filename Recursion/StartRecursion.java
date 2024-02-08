c4eae9d92dbe01b347b5c0b40e1b8a85a765e3da
shahbaz <shahbaz705454@gmail.com>
2024-02-08 20:02:46 +0530

    fibonachi

--- Recursion/StartRecursion.java
+++ Recursion/StartRecursion.java
@@ -3,6 +3,18 @@ package Recursion;
 public class StartRecursion {
 
     public static void main(String[] args) {
-        System.out.println("hello");
+
+        recur(1);
+
+    }
+
+    public static void recur(int n){
+        System.out.println(n);
+
+        if (n==10){
+            return;
+        }
+
+        recur(n+1);
     }
 }
