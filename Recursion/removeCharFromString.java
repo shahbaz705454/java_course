486d30f2a00d1b1ba11fb2307b0f30f5f85de9ca
shahbaz <shahbaz705454@gmail.com>
2024-03-17 14:53:16 +0530

    Subset question

--- Recursion/removeCharFromString.java
+++ Recursion/removeCharFromString.java
@@ -1,5 +1,23 @@
 package Recursion;
 
 public class removeCharFromString {
+    public static void main(String[] args) {
+        remove("","bccadca");
 
+    }
+
+    static void remove(String p,String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+
+        char ch =up.charAt(0);
+
+        if (ch=='a'){
+            remove(p,up.substring(1));
+        }else {
+            remove(p+ch,up.substring(1));
+        }
+    }
 }
---
+++ Subset_Ques/basic.java
@@ -0,0 +1,8 @@
+package Subset_Ques;
+
+public class basic {
+    public static void main(String[] args) {
+        String name ="abc";
+
+    }
+}
