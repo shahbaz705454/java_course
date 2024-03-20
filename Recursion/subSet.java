81785d5592954216430bc07e319d457b42dfcee0
shahbaz <shahbaz705454@gmail.com>
2024-03-20 15:30:01 +0530

    subset and 17 leetcode question question

--- Recursion/subSet.java
+++ Recursion/subSet.java
@@ -1,4 +1,21 @@
 package Recursion;
 
 public class subSet {
+
+    public static void main(String[] args) {
+        String name ="abc";
+        subSeq("",name);
+
+    }
+    public static void subSeq(String p,String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+
+        char ch = up.charAt(0);
+        subSeq(p+ch,up.substring(1));
+        subSeq(p,up.substring(1));
+
+    }
 }
