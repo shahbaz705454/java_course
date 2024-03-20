8e17e78c29a5ffc8a1eec3ac5901f4c89664389a
shahbaz <shahbaz705454@gmail.com>
2024-03-20 20:08:45 +0530

    subset and 17 leetcode question question

--- Recursion/subSet.java
+++ Recursion/subSet.java
@@ -1,10 +1,16 @@
 package Recursion;
 
+import java.util.ArrayList;
+
 public class subSet {
 
     public static void main(String[] args) {
         String name ="abc";
-        subSeq("",name);
+//        subSeq("",name);
+
+
+
+
 
     }
     public static void subSeq(String p,String up){
@@ -16,6 +22,21 @@ public class subSet {
         char ch = up.charAt(0);
         subSeq(p+ch,up.substring(1));
         subSeq(p,up.substring(1));
+
+    }
+
+    static ArrayList<String> subSeqReturn(String p, String up){
+        if(up.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            return list;
+        }
+
+        char ch = up.charAt(0);
+        ArrayList<String> left = subSeqReturn(p+ch,up.substring(1));
+        ArrayList<String> right = subSeqReturn(p,up.substring(1));
+
+        left.addAll(right);
+        return left;
 
     }
 }
