52773403e51a3957f230ecdf5c2ca97a71c799b2
shahbaz <shahbaz705454@gmail.com>
2024-03-20 20:23:07 +0530

    PHONE PROBLEM SOLV

--- Recursion/phonePad.java
+++ Recursion/phonePad.java
@@ -1,27 +1,35 @@
 package Recursion;
 
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class phonePad {
     public static void main(String[] args) {
 
-        pad("","12");
+        System.out.println(pad("","42"));
+
 
 
 
     }
-    public static void pad(String p,String up){
+    static ArrayList<String> pad(String p, String up){
 
         if(up.isEmpty()){
-            System.out.println(p);
-            return;
+            ArrayList<String>list = new ArrayList<>();
+            list.add(p);
+            return list;
         }
+
 
         int digit = up.charAt(0)-'0';
+        ArrayList<String> list = new ArrayList<>();
         for (int i = (digit-1)*3; i <(digit*3) ; i++) {
             char ch = (char) ('a'+i);
-            pad(p+ch,up.substring(1));
-
+            list.addAll(pad(p+ch,up.substring(1)));
 
         }
+        return list;
 
 
 
