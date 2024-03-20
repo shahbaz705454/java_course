c3a39e1f36b71542340bfdf16fc9703c76728c3a
shahbaz <shahbaz705454@gmail.com>
2024-03-20 20:53:22 +0530

    dice game solution

---
+++ Recursion/diceGame.java
@@ -0,0 +1,26 @@
+package Recursion;
+
+public class diceGame {
+    public static void main(String[] args) {
+        dice(" ",4);
+
+    }
+    public static void dice(String p,int target){
+
+        if(target==0){
+            System.out.println(p);
+            return;
+        }
+
+        for (int i = 1; i <=6 && i<=target ; i++) {
+
+            dice(p+i,target-i);
+
+        }
+
+
+
+
+
+    }
+}
