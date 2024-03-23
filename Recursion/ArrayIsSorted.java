d603024df818f048a5344f0ad465a6fa853813ec
shahbaz <shahbaz705454@gmail.com>
2024-03-23 14:25:28 +0530

    Array list recursion question updated

--- Recursion/ArrayIsSorted.java
+++ Recursion/ArrayIsSorted.java
@@ -1,10 +1,14 @@
 package Recursion;
 
+import java.util.ArrayList;
+
 public class ArrayIsSorted {
     public static void main(String[] args) {
 
-        int[] arr = {1, 1,1,1,1,1, 4, 6, 8, 9, 13, 115, 1115};
-        System.out.println(sorted(arr,0));
+        int[] arr = {1, 1,1,1,1,1, 4, 6, 8, 9,4, 13, 115, 1115};
+        int target = 4;
+//        System.out.println(sorted(arr,0));
+        System.out.println(findIndex(arr,target,0,new ArrayList<Integer>()));
 
     }
 
@@ -15,5 +19,21 @@ public class ArrayIsSorted {
        }
 
        return arr[index]<=arr[index+1] && sorted(arr,index+1);
+    }
+
+    static ArrayList<Integer> findIndex(int[] arr,int target,int index,ArrayList<Integer> list){
+
+        if(index == arr.length){
+            return list;
+        }
+
+        if (target==arr[index]){
+          list.add(index);
+
+        }
+
+        return findIndex(arr,target,index+1,list);
+
+
     }
 }
