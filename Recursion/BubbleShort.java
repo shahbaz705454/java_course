2f7cac22f70dabbf23fa08d367a64a719ef33bb2
shahbaz <shahbaz705454@gmail.com>
2024-03-24 21:12:23 +0530

    bubble sort question

--- Recursion/BubbleShort.java
+++ Recursion/BubbleShort.java
@@ -7,9 +7,29 @@ public class BubbleShort {
     public static void main(String[] args) {
 
         int [] arr = { 5,4,3,2,1};
-//        sort(arr,arr.length-1,0);
+        sort(arr,arr.length-1,0);
 
         System.out.println(Arrays.toString(arr));
+
+
+    }
+    public static void sort(int []arr,int r,int c){
+
+        if (r==0){
+            return;
+        }
+
+        if (c<r){
+            if (arr[c]>arr[c+1]){
+                //swap
+                int temp = arr[c+1];
+                arr[c+1]=arr[c];
+                arr[c]=temp;
+            }
+            sort(arr,r,c+1);
+        }else{
+            sort(arr,r-1,0);
+        }
 
 
     }
