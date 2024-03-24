25ea483c0da1890551f970a3d0e5d7442d3d36ed
shahbaz <shahbaz705454@gmail.com>
2024-03-24 20:56:20 +0530

    String Revesrse using recursion

--- Recursion/BubbleShort.java
+++ Recursion/BubbleShort.java
@@ -13,25 +13,7 @@ public class BubbleShort {
 
 
     }
+    public static void
 
-    public static void sort(int[]arr,int r,int c){
-        if(r==0){
-            return;
-        }
 
-        if(c<r){
-
-            if(arr[c]>arr[c+1]){
-                //swap
-                int temp = arr[c];
-                arr[c]= arr[c+1];
-                arr[c+1]=temp;
-            }
-            sort(arr,r,c+1);
-        }else{
-
-            sort(arr,r-1,0);
-        }
-
-    }
 }
--- Trial.java
+++ Trial.java
@@ -1,41 +1,10 @@
 class Solution {
     public static void reverse(char[]a,int s,int e){
 
-        if (s>=e){
-            return;
-        }
-        char temp = a[s];
-        a[s]=a[e];
-        a[e]=temp;
-
-        reverse(a,s+1,e-1);
 
 
 
 
     }
-
-    public static void main(String[] args) {
-        char[] ch = {'h', 'e', 'l', 'l', 'o' };
-        reverseString(ch);
-
-
-    }
-
-    public static void reverseString(char[] s) {
-
-
-        int start = 0;
-        int end = s.length - 1;
-
-        reverse(s, start, end);
-
-        System.out.println(s);
-
-
-    }
-
-
-
 
 }
