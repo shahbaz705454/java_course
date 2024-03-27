1b4cb90e099b831467786d376ef7d6d4a450a756
shahbaz <shahbaz705454@gmail.com>
2024-03-27 14:32:59 +0530

    merge sort modified

--- shorting/Mergesort.java
+++ shorting/Mergesort.java
@@ -1,9 +1,19 @@
 package shorting;
 
+import java.util.Scanner;
+
 public class Mergesort {
 
     public static void main(String[] args) {
-        int arr[] = { 6,3,8,9,2,3,4};
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the array size");
+        int c = sc.nextInt();
+        int arr[] = new int[c];
+        System.out.println("Enters numbers : ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]=sc.nextInt();
+
+        }
         int n = arr.length;
         divide(arr,0,n-1);
         for (int i = 0; i < n; i++) {
