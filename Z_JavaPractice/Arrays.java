050bdf12898a604de852e845ef3be999aca4e752
mohd shahbaz <81930428+shahbaz705454@users.noreply.github.com>
2025-02-01 15:49:07 +0530

    new practice

---
+++ Z_JavaPractice/Arrays.java
@@ -0,0 +1,21 @@
+package Z_JavaPractice;
+
+public class Arrays {
+    public static void main(String[] args) {
+
+        int [] marks = new int[3];
+
+        marks[0] =20;
+        marks[1]=40;
+        marks[2]=99;
+
+        System.out.println(marks);
+
+        for (int i = 0; i < marks.length; i++) {
+            System.out.println(marks[i]);
+        }
+
+        int [] num ={1,2,3,4,5};
+
+    }
+}
---
+++ Z_JavaPractice/Functions.java
@@ -0,0 +1,13 @@
+package Z_JavaPractice;
+
+public class Functions {
+    public static void temp(String a){
+         System.out.println((a));
+    }
+    public static void main(String[] args) {
+        temp("hello");
+
+
+
+    }
+}
